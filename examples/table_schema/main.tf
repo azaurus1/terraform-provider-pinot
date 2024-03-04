bc79994a91c16f3003c2c246401049c941adc86a
hendoxc <hagen.henderson@smartcontract.com>
2024-03-04 14:34:11 +0100

    Running `go generate ./...`

--- examples/table_schema/main.tf
+++ examples/table_schema/main.tf
@@ -9,10 +9,10 @@ terraform {
 
 
 provider "pinot" {
-  controller_url     = "http://localhost:9000"
+  controller_url = "http://localhost:9000"
 }
 
 resource "pinot_schema" "block_schema" {
   schema_name = "ethereum_block_headers"
-  schema = file("block_schema.json")
+  schema      = file("block_schema.json")
 }
