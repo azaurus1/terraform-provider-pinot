fc39866d8756f4131c5d45149d071a22282c2878
Liam Aikin <liamaikin@gmail.com>
2024-03-08 12:30:29 +1100

    adding table example

--- examples/tables/main.tf
+++ examples/tables/main.tf
@@ -8,11 +8,19 @@ terraform {
 
 provider "pinot" {
   controller_url     = "http://localhost:9000"
+  auth_token     = "YWRtaW46dmVyeXNlY3JldA"
+}
+
+resource "pinot_schema" "test_table_schema" {
+  schema_name = "ethereum_mainnet_block_headers"
+  schema     = file("table_schema_example.json")
+  
 }
 
 resource "pinot_table" "test" {
-  table_name = "blockHeader"
+  table_name = "ethereum_mainnet_block_headers"
   table      = file("table_example.json")
+  
 }
 
 # data "pinot_tables" "edu" {
