acca0d04ef6227cb19d49d2efddcd6a5ae47cbab
Liam Aikin <liamaikin@gmail.com>
2024-03-23 23:13:11 +1100

    WIP - Adding Segment resource

--- internal/provider/segments_resource.go
+++ internal/provider/segments_resource.go
@@ -22,6 +22,75 @@ func NewSegmentResource() resource.Resource {
 	return &segmentResource{}
 }
 
+type ContentDisposition struct {
+	Type             string            `tfsdk:"type"`
+	Parameters       map[string]string `tfsdk:"parameters"`
+	FileName         string            `tfsdk:"file_name"`
+	CreationDate     string            `tfsdk:"creation_date"`
+	ModificationDate string            `tfsdk:"modification_date"`
+	ReadDate         string            `tfsdk:"read_date"`
+	Size             int               `tfsdk:"size"`
+}
+
+type MediaType struct {
+	Type            string            `tfsdk:"type"`
+	Subtype         string            `tfsdk:"subtype"`
+	Parameters      map[string]string `tfsdk:"parameters"`
+	WildcardType    bool              `tfsdk:"wildcard_type"`
+	WildcardSubtype bool              `tfsdk:"wildcard_subtype"`
+}
+
+type Parent struct {
+	ContentDisposition   ContentDisposition  `tfsdk:"content_disposition"`
+	Entity               map[string]string   `tfsdk:"entity"`
+	Headers              map[string][]string `tfsdk:"headers"`
+	MediaType            MediaType           `tfsdk:"media_type"`
+	MessageBodyWorkers   map[string]string   `tfsdk:"message_body_workers"`
+	Parent               string              `tfsdk:"parent"`
+	Providers            map[string]string   `tfsdk:"providers"`
+	BodyParts            []map[string]string `tfsdk:"body_parts"`
+	ParameterizedHeaders map[string]string   `tfsdk:"parameterized_headers"`
+}
+
+type BodyPart struct {
+	ContentDisposition   ContentDisposition  `tfsdk:"content_disposition"`
+	Entity               map[string]string   `tfsdk:"entity"`
+	Headers              map[string][]string `tfsdk:"headers"`
+	MediaType            MediaType           `tfsdk:"media_type"`
+	MessageBodyWorkers   map[string]string   `tfsdk:"message_body_workers"`
+	Parent               string              `tfsdk:"parent"`
+	Providers            map[string]string   `tfsdk:"providers"`
+	ParameterizedHeaders map[string]string   `tfsdk:"parameterized_headers"`
+}
+
+type Field struct {
+	ContentDisposition         ContentDisposition  `tfsdk:"content_disposition"`
+	Entity                     map[string]string   `tfsdk:"entity"`
+	Headers                    map[string][]string `tfsdk:"headers"`
+	MediaType                  MediaType           `tfsdk:"media_type"`
+	MessageBodyWorkers         map[string]string   `tfsdk:"message_body_workers"`
+	Parent                     string              `tfsdk:"parent"`
+	Providers                  map[string]string   `tfsdk:"providers"`
+	Name                       string              `tfsdk:"name"`
+	Value                      string              `tfsdk:"value"`
+	Simple                     bool                `tfsdk:"simple"`
+	FormDataContentDisposition ContentDisposition  `tfsdk:"form_data_content_disposition"`
+	ParameterizedHeaders       map[string]string   `tfsdk:"parameterized_headers"`
+}
+
+type segementResourceModel struct {
+	ContentDisposition   ContentDisposition  `tfsdk:"content_disposition"`
+	Entity               map[string]string   `tfsdk:"entity"`
+	Headers              map[string][]string `tfsdk:"headers"`
+	MediaType            MediaType           `tfsdk:"media_type"`
+	MessageBodyWorkers   map[string]string   `tfsdk:"message_body_workers"`
+	Parent               Parent              `tfsdk:"parent"`
+	Providers            map[string]string   `tfsdk:"providers"`
+	BodyParts            []BodyPart          `tfsdk:"body_parts"`
+	Fields               []Field             `tfsdk:"fields"`
+	ParameterizedHeaders map[string]string   `tfsdk:"parameterized_headers"`
+}
+
 func (t *segmentResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
 	if req.ProviderData == nil {
 		return
@@ -170,11 +239,124 @@ func (t *segmentResource) Schema(_ context.Context, _ resource.SchemaRequest, re
 					},
 				},
 			},
+			"providers": schema.MapAttribute{
+				ElementType: basetypes.StringType{},
+			},
+			"body_parts": schema.ListNestedAttribute{
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"content_disposition": schema.SingleNestedAttribute{
+							Attributes: map[string]schema.Attribute{
+								"type": schema.StringAttribute{},
+								"parameters": schema.MapAttribute{
+									ElementType: basetypes.StringType{},
+								},
+								"file_name":         schema.StringAttribute{},
+								"creation_date":     schema.StringAttribute{},
+								"modification_date": schema.StringAttribute{},
+								"read_date":         schema.StringAttribute{},
+								"size":              schema.NumberAttribute{},
+							},
+						},
+						"entity": schema.MapAttribute{
+							ElementType: basetypes.StringType{},
+						},
+						"headers": schema.MapAttribute{
+							ElementType: basetypes.ListType{},
+						},
+						"media_type": schema.SingleNestedAttribute{
+							Attributes: map[string]schema.Attribute{
+								"type":    schema.StringAttribute{},
+								"subtype": schema.StringAttribute{},
+								"parameters": schema.MapAttribute{
+									ElementType: basetypes.StringType{},
+								},
+								"wildcard_type":    schema.BoolAttribute{},
+								"wildcard_subtype": schema.BoolAttribute{},
+							},
+						},
+						"message_body_workers": schema.MapAttribute{
+							ElementType: basetypes.StringType{},
+						},
+						"parent": schema.StringAttribute{},
+						"providers": schema.MapAttribute{
+							ElementType: basetypes.StringType{},
+						},
+						"parameterized_headers": schema.MapAttribute{
+							ElementType: basetypes.StringType{},
+						},
+					},
+				},
+			},
+			"fields": schema.MapNestedAttribute{
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"content_disposition": schema.SingleNestedAttribute{
+							Attributes: map[string]schema.Attribute{
+								"type": schema.StringAttribute{},
+								"parameters": schema.MapAttribute{
+									ElementType: basetypes.StringType{},
+								},
+								"file_name":         schema.StringAttribute{},
+								"creation_date":     schema.StringAttribute{},
+								"modification_date": schema.StringAttribute{},
+								"read_date":         schema.StringAttribute{},
+								"size":              schema.NumberAttribute{},
+							},
+						},
+						"entity": schema.MapAttribute{
+							ElementType: basetypes.StringType{},
+						},
+						"headers": schema.MapAttribute{
+							ElementType: basetypes.ListType{},
+						},
+						"media_type": schema.SingleNestedAttribute{
+							Attributes: map[string]schema.Attribute{
+								"type":    schema.StringAttribute{},
+								"subtype": schema.StringAttribute{},
+								"parameters": schema.MapAttribute{
+									ElementType: basetypes.StringType{},
+								},
+								"wildcard_type":    schema.BoolAttribute{},
+								"wildcard_subtype": schema.BoolAttribute{},
+							},
+						},
+						"message_body_workers": schema.MapAttribute{
+							ElementType: basetypes.StringType{},
+						},
+						"parent": schema.StringAttribute{},
+						"providers": schema.MapAttribute{
+							ElementType: basetypes.StringType{},
+						},
+						"name":   schema.StringAttribute{},
+						"value":  schema.StringAttribute{},
+						"simple": schema.BoolAttribute{},
+						"form_data_content_disposition": schema.SingleNestedAttribute{
+							Attributes: map[string]schema.Attribute{
+								"type": schema.StringAttribute{},
+								"parameters": schema.MapAttribute{
+									ElementType: basetypes.StringType{},
+								},
+								"file_name":         schema.StringAttribute{},
+								"creation_date":     schema.StringAttribute{},
+								"modification_date": schema.StringAttribute{},
+								"read_date":         schema.StringAttribute{},
+								"size":              schema.NumberAttribute{},
+							},
+						},
+						"parameterized_headers": schema.MapAttribute{
+							ElementType: basetypes.StringType{},
+						},
+					},
+				},
+			},
+			"parameterized_headers": schema.MapAttribute{},
 		},
 	}
 }
 
 func (t *segmentResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan segmentResourceModel
 }
 
 func (t *segmentResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
