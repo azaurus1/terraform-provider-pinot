9fbb65d385225984b2f1615e05fdd3490ee66137
Liam Aikin <liamaikin@gmail.com>
2024-04-03 15:11:26 +1100

    added pinot-testcontainers for running pinot containers for testing

--- internal/provider/users_resource_test.go
+++ internal/provider/users_resource_test.go
@@ -1,12 +1,34 @@
 package provider
 
 import (
+	"context"
+	"fmt"
 	"testing"
+	"time"
 
+	pinot_testContainer "github.com/azaurus1/pinot-testContainer"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccUsersResource(t *testing.T) {
+
+	context, cancel := context.WithTimeout(context.TODO(), 5*time.Minute)
+	defer cancel()
+
+	pinot, err := pinot_testContainer.RunPinotContainer(context)
+	if err != nil {
+		t.Fatalf("Failed to run Pinot container: %v", err)
+	}
+
+	fmt.Println(pinot.URI)
+
+	providerConfig := fmt.Sprintf(`
+provider "pinot" {
+	controller_url = "http://%s"
+	auth_token = "YWRtaW46dmVyeXNlY3JldA"
+}
+`, pinot.URI)
+
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
