db18d7b28c12604b9e465cd576791ed5e15e9f5a
Liam Aikin <liamaikin@gmail.com>
2024-04-06 18:49:00 +1100

    added schema resource acceptance test

--- examples/table_schema/main.tf
+++ examples/table_schema/main.tf
@@ -18,9 +18,11 @@ resource "pinot_schema" "block_schema" {
   date_time_field_specs = [{
     data_type   = "LONG",
     name        = "block_timestamp",
+    not_null    = false,
     format      = "1:MILLISECONDS:EPOCH",
     granularity = "1:MILLISECONDS",
   }]
+  enable_column_based_null_handling = false
   dimension_field_specs = [{
     name      = "block_number",
     data_type = "INT",
